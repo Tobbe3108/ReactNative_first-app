8a400b3afeb5fe909274fec450df5ec0b38074e0
Tobbe3108 <Tobbe3108@gmail.com>
2019-11-02 14:27:43 +0100

    init

---
+++ App.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  const [outputText, setOutputText] = useState('Hello World!');
+  return (
+    <View style={styles.container}>
+      <Text>{outputText}</Text>
+      <Button
+        title='Change Text'
+        onPress={() => setOutputText('The text changed!')}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
